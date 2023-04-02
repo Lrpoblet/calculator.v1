832470927dea11b9ce664f4b99c78926f92795bd
Lrpoblet <lrpoblet@gmail.com>
2023-04-02 15:12:31 +0200

    add iphone calculator structure and styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,90 +1,40 @@
 import { useState } from 'react';
 import '../styles/App.css';
+import Button from './Button';
 
 const App = () => {
-  const [numberA, setNumberA] = useState(0);
-  const [numberB, setNumberB] = useState(0);
-  const [op, setOp] = useState('suma');
-  let total = 0;
-
-  const handleNumberA = (ev) => {
-    const inputValue = ev.target.value;
-    setNumberA(inputValue);
-  };
-
-  const handleNumberB = (ev) => {
-    setNumberB(ev.target.value);
-  };
-
-  const handleOp = (ev) => {
-    setOp(ev.target.value);
-  };
-
-  const handleReset = () => {
-    setOp('suma');
-    setNumberA(0);
-    setNumberB(0);
-    total = 0;
-  };
-
-  const calc = () => {
-    if (op === 'suma') {
-      total = parseInt(numberA) + parseInt(numberB);
-    } else if (op === 'resta') {
-      total = parseInt(numberA) - parseInt(numberB);
-    } else if (op === 'multiplicación') {
-      total = parseInt(numberA) * parseInt(numberB);
-    } else if (op === 'división') {
-      total = parseInt(numberA) / parseInt(numberB);
-    }
-    return total;
+  const handleButtonPress = () => {
+    console.log('handle');
   };
 
   return (
-    <div className="body">
-      <h1 className="title">Calculadora</h1>
-      <form className="calc">
-        <label className="label" htmlFor="op">
-          Operación:
-          <select
-            className="box"
-            name="op"
-            id="op"
-            value={op}
-            onChange={handleOp}
-          >
-            <option value="suma">suma</option>
-            <option value="resta">resta</option>
-            <option value="multiplicación">multiplicación</option>
-            <option value="división">división</option>
-          </select>
-        </label>
-        <label className="label">
-          Escribe un número:
-          <input
-            className="box"
-            type="number"
-            onChange={handleNumberA}
-            value={numberA}
-          />
-        </label>
-        <label className="label">
-          Escribe otro número:
-          <input
-            className="box"
-            type="number"
-            onChange={handleNumberB}
-            value={numberB}
-          />
-        </label>
-      </form>
-      <p className="result">
-        La {op} de <strong>{numberA}</strong> y <strong>{numberB}</strong> es
-        <strong> {calc()}</strong>.
-      </p>
-      <button className="reset" onClick={handleReset}>
-        reset
-      </button>
+    <div className="App">
+      <div className="display">Resultado</div>
+      <div className="buttons">
+        <Button onButtonClick={handleButtonPress} content="C" type="function" />
+        <Button
+          onButtonClick={handleButtonPress}
+          content="+/-"
+          type="function"
+        />
+        <Button onButtonClick={handleButtonPress} content="%" type="function" />
+        <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="7" />
+        <Button onButtonClick={handleButtonPress} content="8" />
+        <Button onButtonClick={handleButtonPress} content="9" />
+        <Button onButtonClick={handleButtonPress} content="×" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="4" />
+        <Button onButtonClick={handleButtonPress} content="5" />
+        <Button onButtonClick={handleButtonPress} content="6" />
+        <Button onButtonClick={handleButtonPress} content="−" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="1" />
+        <Button onButtonClick={handleButtonPress} content="2" />
+        <Button onButtonClick={handleButtonPress} content="3" />
+        <Button onButtonClick={handleButtonPress} content="+" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="0" />
+        <Button onButtonClick={handleButtonPress} content="." />
+        <Button onButtonClick={handleButtonPress} content="=" type="operator" />
+      </div>
     </div>
   );
 };
